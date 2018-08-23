607d35e78571cbc3a8e1efb657244f22efb72c52
lvxiaozheng <2293548359@qq.com>
2018-08-23 18:03:14 +0800

    20180823A

--- src/main/java/com/prd/PrdservletApplication.java
+++ src/main/java/com/prd/PrdservletApplication.java
@@ -3,8 +3,6 @@ package com.prd;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.prd.module.*.dao")
